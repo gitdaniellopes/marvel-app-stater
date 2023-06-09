3e615ad428c4913fd0924d2df0791b2eec4045b1
ingridolitk <70184804+luizsfl@users.noreply.github.com>
2023-06-08 21:17:57 -0300

    listagem recycleview sendo exibida

--- app/src/main/java/daniel/lop/io/marvelappstarter/utils/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/utils/Constants.kt
@@ -1,7 +1,7 @@
 package daniel.lop.io.marvelappstarter.utils
 
 object Constants {
-    const val BASE_URL ="https://gateway.marvel.com/V1/public/"
+    const val BASE_URL ="https://gateway.marvel.com/v1/public/"
     const val PUBLIC_KEY = "767bf566fe6415307cd8b370998457b4"
     const val PRIVATE_KEY = "16baf1d95bdcabcef9c07c258e3fe052977e8438"
     const val APIKEY = "apikey"
